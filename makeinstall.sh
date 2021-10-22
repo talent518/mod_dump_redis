a624feaa37375536dc46f7dc60eefa5a17ed0bc5
阿宝 <talent518@yeah.net>
2021-10-23 03:35:53 +0800

    Update makeinstall.sh

--- makeinstall.sh
+++ makeinstall.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/sh
 
-lampp stopapache
-
 rm -rf .libs *.o *.slo *.lo *.la
 
 apxs -cia -l m -n dump_redis mod_dump_redis.c redis.c
-
-chown daemon.daemon /opt/lampp/modules/mod_dump_redis.so
-
-lampp startapache
 
 gcc -o redis-dump -lm dump.c redis.c
 
